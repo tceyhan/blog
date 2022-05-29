8eea67c1677c1636c10659f7a7ba04495220fc4e
tceyhan <55tc155@gmail.com>
2022-05-29 20:26:35 +0300

    project started

---
+++ users/forms.py
@@ -0,0 +1,8 @@
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserForm(UserCreationForm):
+    class Meta():
+        model = User
+        fields = ('username', 'password1', 'password2', 'portfolio', 'profile_pic', 'first_name', 'last_name')
+        
---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    portfolio = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    username =models.EmailField("Email Address", unique=True)
+    REQUIRED_FIELDS =[] 
+    # emaili sormasın diye boş bıraktık
