b2673cd9577bda8b4c40e1fbb64fc79318f3f0a1
tceyhan <55tc155@gmail.com>
2022-06-02 03:46:00 +0300

    register html changed

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,10 @@ from django import forms
 class UserForm(UserCreationForm):
     class Meta():
         model = User
-        fields = ('username', 'password1', 'password2', 'portfolio', 'profile_pic', 'first_name', 'last_name')
+        fields = ('username', 'password1', 'password2', 'profile_pic', 'first_name', 'last_name')
+        widgets = {            
+            'profile_pic': forms.FileInput(attrs={'class': 'form-control bg-info'}),
+        } 
 
 class UserProfileForm(forms.ModelForm):
     class Meta():
