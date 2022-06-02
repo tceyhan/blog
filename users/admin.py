c81d6ce9c5aad2d882a3658e04341f6a7b373c5d
tceyhan <55tc155@gmail.com>
2022-06-03 02:11:22 +0300

    models changed

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import User
+from .models import User, UserProfile
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name')    
+    list_display = ('username', 'first_name', 'last_name', 'email', 'date_joined',)    
     search_fields = ['username', 'first_name']
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'profile_pic')
+    search_fields = ['user']
+
 admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile)
+
+
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import register,user_logout,user_login, user_profile, about
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('register/', register, name='register' ),
@@ -7,4 +10,4 @@ urlpatterns = [
     path('login/', user_login, name='user_login'),
     path('profile/', user_profile, name='user_profile'),
     path('about/', about, name='about'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
